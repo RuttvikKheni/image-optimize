a65516402c0293217aff72a24a386521bee74be8
RuttvikKheni <developer.ruttvikkheni@gmail.com>
2023-02-24 15:23:04 +0530

    check image!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,48 +13,36 @@ export default function Home() {
         height="500"
         width="300"
         alt="test"
-        unoptimized
-        priority
       />
       <Image
         src="https://tinypic.host/images/2022/12/21/9845g459gijj459.th.jpg"
         height="500"
         width="300"
         alt="test"
-        unoptimized
-        priority
       />
       <Image
         src="https://tinypic.host/images/2022/12/21/9845g459gijj459.th.jpg"
         height="500"
         width="300"
         alt="test"
-        unoptimized
-        priority
       />
       <Image
         src="https://tinypic.host/images/2023/02/19/8-Who-Made-Me-A-Princess-Chapter-35---DuaTeman.th.jpg"
         height="100"
         width="400"
         alt="test"
-        unoptimized
-        priority
       />
       <Image
         src="https://tinypic.host/images/2023/02/23/IMG_7690.th.jpg"
         height="100"
         width="400"
         alt="test"
-        unoptimized
-        priority
       />
       <Image
         src="https://tinypic.host/images/2022/06/23/h.th.png"
         height="100"
         width="400"
         alt="test"
-        unoptimized
-        priority
       />
       <Image
         src="https://tinypic.host/images/2022/10/12/image022.th.jpg"
