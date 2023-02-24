819d25513964a3c80c368dbe41223bbb6c66e7c3
RuttvikKheni <developer.ruttvikkheni@gmail.com>
2023-02-24 12:55:02 +0530

    check image optimize

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,48 +25,36 @@ export default function Home() {
         height="500"
         width="300"
         alt="test"
-        priority
-        unoptimized
       />
       <Image
         src="https://tinypic.host/images/2023/02/13/instantnoodle2303.th.jpg"
         height="100"
         width="400"
         alt="test"
-        priority
-        unoptimized
       />
       <Image
         src="https://tinypic.host/images/2023/02/07/P-UNLV046.th.jpg"
         height="100"
         width="400"
         alt="test"
-        priority
-        unoptimized
       />
       <Image
         src="https://tinypic.host/images/2022/02/18/Kimber-Mags.th.jpg"
         height="100"
         width="400"
         alt="test"
-        priority
-        unoptimized
       />
       <Image
         src="https://tinypic.host/images/2022/10/31/Custom-Floor-Signs.th.jpg"
         height="100"
         width="400"
         alt="test"
-        priority
-        unoptimized
       />
       <Image
         src="https://tinypic.host/images/2023/02/21/export202302210845517160.png"
         height="100"
         width="400"
         alt="test"
-        priority
-        unoptimized
       />
     </>
   );
