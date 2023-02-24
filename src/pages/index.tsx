424010abc24404f6edbec5758aaefa5d4d4a6b48
RuttvikKheni <developer.ruttvikkheni@gmail.com>
2023-02-24 11:59:47 +0530

    check image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,101 +9,67 @@ export default function Home() {
   return (
     <>
       <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
+        src="https://tinypic.host/images/2023/01/17/DSC00455_zps03ba7e02.th.jpg"
         height="500"
         width="300"
         alt="test"
         unoptimized
+        priority
       />
       <Image
         src="https://tinypic.host/images/2023/02/04/515454515.jpg"
         height="500"
         width="300"
         alt="test"
+        priority
         unoptimized
       />
       <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
+        src="https://tinypic.host/images/2023/02/22/imagedb33e64846805f71.th.png"
         height="500"
         width="300"
         alt="test"
+        priority
         unoptimized
       />
       <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
-        height="500"
-        width="300"
-        alt="test"
-        unoptimized
-      />
-      <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
-        height="500"
-        width="300"
-        alt="test"
-        unoptimized
-      />
-      <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
-        height="500"
-        width="300"
-        alt="test"
-        unoptimized
-      />
-      <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
-        height="500"
-        width="300"
-        alt="test"
-        unoptimized
-      />
-      <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
-        height="500"
-        width="300"
-        alt="test"
-        unoptimized
-      />
-      <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
-        height="500"
-        width="300"
-        alt="test"
-        unoptimized
-      />
-      <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
+        src="https://tinypic.host/images/2023/02/13/instantnoodle2303.th.jpg"
         height="100"
         width="400"
         alt="test"
+        priority
         unoptimized
       />
       <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
+        src="https://tinypic.host/images/2023/02/07/P-UNLV046.th.jpg"
         height="100"
         width="400"
         alt="test"
+        priority
         unoptimized
       />
       <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
+        src="https://tinypic.host/images/2022/02/18/Kimber-Mags.th.jpg"
         height="100"
         width="400"
         alt="test"
+        priority
         unoptimized
       />
       <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
+        src="https://tinypic.host/images/2022/10/31/Custom-Floor-Signs.th.jpg"
         height="100"
         width="400"
         alt="test"
+        priority
         unoptimized
       />
       <Image
-        src="https://tinypic.host/images/2023/02/04/515454515.jpg"
+        src="https://tinypic.host/images/2023/02/21/export202302210845517160.png"
         height="100"
         width="400"
         alt="test"
+        priority
         unoptimized
       />
     </>
